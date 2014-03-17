db82621c26fc282852ce31a7d37bc37ee599c5b8
rohatiro <rohatiro@gmail.com>
2014-03-16 22:09:27 -0600

    Ejemplo de servidor que sirve paginas HTML

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-var express, server;
+var express, server, swig, path, root, approot, publicdir;
 
+//plugins
+path = require('path');
 express = require('express');
-server = express();
+swig = require('swig');
 
-server.get('/', function (req, res) {
-	res.send('Hola Mundo');
+//direcciones
+root = path.resolve(__dirname,'.');
+publicdir = path.resolve(root, 'public/');
+approot = path.resolve(root, 'app/');
+views = path.resolve(approot, 'views/');
+
+//configuracion
+server = express();
+server.engine('html', swig.renderFile);
+server.set('view engine', 'html');
+server.set('views', views);
+server.configure(function () {
+	server.use(express.static(publicdir));
+	server.use(express.logger());
+	server.use(express.cookieParser());
+	server.use(express.bodyParser());
 });
 
+//rutas
+server.get('/', function (req, res) {
+	res.render('index');
+});
+
+//Inicio del servidor
 server.listen(3000);
+console.log('El servidor se ha iniciado en el puerto 3000');
